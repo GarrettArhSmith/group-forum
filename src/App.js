6809ef63cb12f205380dda4cb80d3c1c461a1ce4
Your Name <you@example.com>
2020-10-01 14:19:33 -0600

    Tylers commit first branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +15,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <p>
+            Practice
+          </p>
         </a>
       </header>
     </div>
